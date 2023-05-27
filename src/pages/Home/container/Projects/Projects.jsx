3ccd615b434903d4e7548efad513adb3f8bc60b9
Jerry <jerryfu_mc@outlook.com>
2023-05-27 19:45:32 -0400

    update [skip ci]

--- src/pages/Home/container/Projects/Projects.jsx
+++ src/pages/Home/container/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {motion} from "framer-motion";
 import {images} from "../../../../constants";
 
@@ -6,11 +6,63 @@ import {AppWrap} from "../../../components/index.js";
 
 import "./Projects.scss";
 
+
+const categories = [
+    {
+        name: "All",
+        description: "",
+        imageUrl: ""
+    },
+    {
+        name: "JavaScript",
+        description: "",
+        imageUrl: ""
+    },
+    {
+        name: "Python",
+        description: "",
+        imageUrl: ""
+    },
+    {
+        name: "Java",
+        description: "",
+        imageUrl: ""
+    }
+]
+
 const Projects = () => {
+    const [activeFilter, setActiveFilter] = useState("All");
+    const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1});
+    const [filterProject, setFilterProject] = useState([]);
+
+
+    const handleFilter = (category) => {
+
+    }
+
     return (
         <>
             <h2 className="head-text"><span>Pro</span>jects</h2>
-            <p className="text">💭</p>
+            <div className="app__projects_filter">
+                {categories.map((category) => (
+                    <div
+                        key={category.name}
+                        onClick={() => handleFilter(category)}
+                        className={`app__projects_filter-item app__flex p-text ${activeFilter === category ? "item-active" : ""}`}
+
+                    >
+                        {category.name}
+                    </div>
+                ))}
+            </div>
+
+            <motion.div
+                animate={animateCard}
+                transition={{duration: 0.50, delayChildren: 0.50}}
+                className="app__project-portfolio"
+            >
+
+            </motion.div>
         </>
     );
 };
